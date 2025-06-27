dbc481430bcb1e27a9c8ea7f9644068d52dea14e
Kai <kai@sbiz.cc>
2025-06-27 15:18:57 +0700

    Logic check role admin-user

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,16 +1,19 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { UsersService } from '@/modules/users/users.service';
 import { comparePasswordHelper } from '@/helpers/util';
 import { JwtService } from '@nestjs/jwt';
-import { ChangePasswordAuthDto, CodeAuthDto, CreateAuthDto } from './dto/create-auth.dto';
+import {
+  ChangePasswordAuthDto,
+  CodeAuthDto,
+  CreateAuthDto,
+} from './dto/create-auth.dto';
 
 @Injectable()
 export class AuthService {
   constructor(
     private usersService: UsersService,
-    private jwtService: JwtService
-  ) { }
-
+    private jwtService: JwtService,
+  ) {}
 
   async validateUser(username: string, pass: string): Promise<any> {
     const user = await this.usersService.findByEmail(username);
@@ -27,7 +30,8 @@ export class AuthService {
       user: {
         email: user.email,
         _id: user._id,
-        name: user.name
+        name: user.name,
+        role: user.role,
       },
       access_token: this.jwtService.sign(payload),
     };
@@ -35,22 +39,21 @@ export class AuthService {
 
   handleRegister = async (registerDto: CreateAuthDto) => {
     return await this.usersService.handleRegister(registerDto);
-  }
+  };
 
   checkCode = async (data: CodeAuthDto) => {
     return await this.usersService.handleActive(data);
-  }
+  };
 
   retryActive = async (data: string) => {
     return await this.usersService.retryActive(data);
-  }
+  };
 
   retryPassword = async (data: string) => {
     return await this.usersService.retryPassword(data);
-  }
+  };
 
   changePassword = async (data: ChangePasswordAuthDto) => {
     return await this.usersService.changePassword(data);
-  }
-
+  };
 }
